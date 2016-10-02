928deecc7b22d76a4a20d9cff0bade14842b9223
Mitchell Henke <mitchh23@gmail.com>
2016-10-02 15:59:43 -0500

    fix naming

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ const ROOT_URL = 'http://localhost:4000';
 export const FETCH_PROPERTIES = 'FETCH_PROPERTIES';
 
 export function fetchProperties(p) {
-  const url = `${ROOT_URL}?latitude=${p.latitude}&longitude=${p.longitude}&radius=${p.radius}&minBathrooms=${p.minBathrooms}&maxBathrooms=${p.maxBathrooms}&minBedrooms=${p.minBedrooms}&maxBedrooms=${p.maxBedrooms}&zipcode=${p.zipcode}&land_use=${p.land_use}&parking_type=${p.parking_type}&number_units=${p.numberUnits}`;
+  const url = `${ROOT_URL}?latitude=${p.latitude}&longitude=${p.longitude}&radius=${p.radius}&minBathrooms=${p.minBathrooms}&maxBathrooms=${p.maxBathrooms}&minBedrooms=${p.minBedrooms}&maxBedrooms=${p.maxBedrooms}&zipcode=${p.zipcode}&land_use=${p.land_use}&parking_type=${p.parking_type}&number_units=${p.number_units}`;
   const request = axios.get(url);
 
   return {
