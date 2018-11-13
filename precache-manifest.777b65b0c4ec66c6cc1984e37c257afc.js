ff935cfc1b83e15a1a781f6a5f4c39d582328195
Mitchell Henke <mitchh23@gmail.com>
2018-11-13 09:22:20 -0600

    new release

---
+++ precache-manifest.777b65b0c4ec66c6cc1984e37c257afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bdbd59c6f15993c8d6f",
+    "url": "/static/js/main.9bdbd59c.chunk.js"
+  },
+  {
+    "revision": "097a7fa47e21acdf83c3",
+    "url": "/static/js/1.097a7fa4.chunk.js"
+  },
+  {
+    "revision": "9bdbd59c6f15993c8d6f",
+    "url": "/static/css/main.b9037962.chunk.css"
+  },
+  {
+    "revision": "097a7fa47e21acdf83c3",
+    "url": "/static/css/1.f49b4e41.chunk.css"
+  },
+  {
+    "revision": "53ec4176ea570428b73b286643f16273",
+    "url": "/index.html"
+  }
+];
